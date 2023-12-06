5e4378321a4b947bf8d4089756871cf9a7c93420
vaughankraska <vaughankraska@gmail.com>
2023-12-06 22:25:26 +0100

    boosted model with analysis df xg_boost_optimal_results.pkl

--- src/utils.py
+++ src/utils.py
@@ -66,7 +66,7 @@ def cross_validate_model(pipeline, X, y, n_splits=5, scoring='accuracy', cpu_cou
     from sklearn.model_selection import cross_val_score, KFold
 
     kf = KFold(n_splits=n_splits, shuffle=True, random_state=123)
-    cvs = cross_val_score(pipeline, X, y, cv=kf, scoring='accuracy', n_jobs=cpu_count)
+    cvs = cross_val_score(pipeline, X, y, cv=kf, scoring=scoring, n_jobs=cpu_count)
 
     return cvs
 
