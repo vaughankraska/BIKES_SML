e5f36173b8695f32d06d99f5cf8b6b7b5993a310
Ellaa-s <ella.schmidtobreick.4283@student.uu.se>
2023-12-07 17:58:25 +0100

    creation of weather_score

---
+++ final/weather_score_creation.py
@@ -0,0 +1,28 @@
+# Load packages
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+# Load training data
+data = pd.read_csv(r"../data/training_data.csv")
+
+data = data.drop('snow', axis=1) #no information in this column
+data['is_high_demand'] = data['increase_stock'].apply(lambda entity: 1 if entity == 'high_bike_demand' else 0)
+data['is_high_demand'] = data['is_high_demand'].astype('int')
+
+# Train a linear regression model to get the coefficients used in weather_score
+scaler = MinMaxScaler()
+y = data['is_high_demand']
+data_weather = data.drop(
+    columns=['is_high_demand', 'increase_stock', 'hour_of_day', 'day_of_week', 'month', 'holiday', 'weekday'])
+X = scaler.fit_transform(data_weather)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, random_state=123)
+
+weather_model = LinearRegression()
+weather_model.fit(X_train, y_train)
+
+#
+print(weather_model.intercept_)
+print(weather_model.coef_)
