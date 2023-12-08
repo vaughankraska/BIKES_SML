cab95272750aa13c7c92bf3859b7af29c459ae8c
Ellaa-s <ella.schmidtobreick.4283@student.uu.se>
2023-12-08 10:53:43 +0100

    added pandas

--- src/weather_score_creation.py
+++ src/weather_score_creation.py
@@ -1,6 +1,7 @@
 # weather_score_creation.py
 
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
