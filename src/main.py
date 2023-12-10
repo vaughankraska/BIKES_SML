a330d1f31aca044e1721c8632dac51cc14b05b00
vaughankraska <vaughankraska@gmail.com>
2023-12-10 23:13:19 +0100

    main.py runs predixt on any new data using the final_model.pkl model

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print('hello world')
+# main.py
+def final_model_predict(new_data_x):
+    from pickle import load
+
+    # new_data = pd.read_csv('./new_data_that_we_dont_have_yet.csv'):
+    with open('./final_model.pkl', 'rb') as file:
+        final_model = load(file)
+
+    return final_model.predict(new_data_x)
